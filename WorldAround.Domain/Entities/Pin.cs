b21e4fea66db317dd468afe486d25e0f585c2650
oleksslob <slobodian.oleksandr.d@chnu.edu.ua>
2022-05-24 10:46:53 +0300

    Removed redundant usings and changed db name

--- WorldAround.Domain/Entities/Pin.cs
+++ WorldAround.Domain/Entities/Pin.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WorldAround.Domain.Entities
+﻿namespace WorldAround.Domain.Entities
 {
     public class Pin
     {
