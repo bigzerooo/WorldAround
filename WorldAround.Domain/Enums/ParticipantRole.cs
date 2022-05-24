1b8b029e49a5936cc8e01e1d3c9b633ef3ca99ca
oleksslob <slobodian.oleksandr.d@chnu.edu.ua>
2022-05-24 21:36:19 +0300

    Added AspNetIdentity

--- WorldAround.Domain/Enums/ParticipantRole.cs
+++ WorldAround.Domain/Enums/ParticipantRole.cs
@@ -4,5 +4,5 @@ public enum ParticipantRole
 {
     Owner,
     Moderator,
-    Subcriber
+    Subscriber
 }
--- WorldAround.Infrastructure/Data/WorldAroundDbContext.cs
+++ WorldAround.Infrastructure/Data/WorldAroundDbContext.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using WorldAround.Application.Interfaces.Infrastructure;
 using WorldAround.Domain.Entities;
 
 namespace WorldAround.Infrastructure.Data;
 
-public class WorldAroundDbContext : DbContext, IWorldAroundDbContext
+public class WorldAroundDbContext : IdentityDbContext<User, IdentityRole<int>, int>, IWorldAroundDbContext
 {
     public DbSet<Attraction> Attractions { get; set; }
-    public DbSet<Event> Events{ get; set; }
+    public DbSet<Event> Events { get; set; }
     public DbSet<Participant> Participants { get; set; }
     public DbSet<Pin> Pins { get; set; }
     public DbSet<Trip> Trips { get; set; }
