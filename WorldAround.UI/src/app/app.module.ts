59c5dbd9ca663da39f899f5f70c0de6a836fcc9b
Skibinskyi Denys <ruslanskibinskii@gmail.com>
2022-05-24 19:40:33 +0300

    Added header on ui part 1

--- WorldAround.UI/src/app/app.module.ts
+++ WorldAround.UI/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TripsGridComponent } from './components/trips-grid/trips-grid.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TripsGridComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
