06266ac66b863e87dd9b8d0641fbedad2d334f85
oleksslob <slobodian.oleksandr.d@chnu.edu.ua>
2022-06-27 00:18:24 +0300

    Added unit test project

---
+++ WorldAround.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace WorldAround.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
